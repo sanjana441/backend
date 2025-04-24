699862d1d9731ce8267c34ca57587beaaa3fec8b
sanjana441 <sanjana.kumari12102004@gmail.com>
2025-04-24 15:41:41 +0530

    "add user and video model"

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 import dotenv from "dotenv";
 dotenv.config({path:'./env'});
 import connectdb from "./db/index.js";
-connectdb()
+connectdb().then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`server running on the port ${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log("Mongodb connection failed!!!!",err);
+})
